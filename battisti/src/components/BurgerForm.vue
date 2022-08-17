2baa3cb756efbc8d773cfc1e398603f77db136a4
Anderson Coimbra <andersonmegax@hotmail.com>
2022-08-17 01:39:38 -0300

    Conclusão do curso de VUE3

---
+++ battisti/src/components/BurgerForm.vue
@@ -0,0 +1,165 @@
+<template>
+    <div >
+       <Message :msg="msg" v-show="msg" />
+       <form id="burger-form" @submit="createBurger">
+        <div class="input-container">
+            <label for="name">Nome</label>
+            <input type="text" id="name" v-model="name" />
+        </div>
+        <div class="input-container">
+            <label for="pao">Escolha o seu pão</label>
+            <select id="pao" v-model="pao">
+                <option value="" >Selecione o Pão</option>
+                <option v-for="pao in paes" :value="pao.tipo" :key="pao.id">{{pao.tipo}}</option>
+            </select>
+        </div>
+        <div class="input-container">
+            <label for="carne">Escolha a carne do seu Burger</label>
+            <select id="carne" v-model="carne">
+                <option value="" >Selecione o tipo de Carne</option>
+                <option v-for="carne in carnes" :value="carne.tipo" :key="carne.id">{{carne.tipo}}</option>
+            </select>
+        </div>
+        <div id="opcionais-container" class="input-container">
+            <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
+            <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id" >
+                <input type="checkbox" name="salame" v-model="opcionais" :value="opcional.tipo" />
+                <span>{{opcional.tipo}}</span>
+            </div>
+        </div>
+        <div class="input-container">
+            <input type="submit" class="submit-btn" value="Criar meu burger">
+        </div>
+       </form>
+    </div>
+</template>
+<script>
+import Message from '../components/Message.vue';
+export default {
+    name: 'BurgerForm',
+    data() {
+        return {
+            paes: null,
+            carnes: null,
+            opcionaisdata: null,
+            name: null,
+            pao: null,
+            carne: null,
+            opcionais: [],
+            msg: null
+        };
+    },
+    methods:{
+        async getIngredientes(){
+            const req = await fetch('http://localhost:3000/ingredientes');
+            const data = await req.json();
+            
+            this.paes = data.paes;
+            this.carnes = data.carnes;
+            this.opcionaisdata = data.opcionais;
+        },
+        async createBurger(e){
+            e.preventDefault();
+            const req = await fetch('http://localhost:3000/burgers', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: this.name,
+                    pao: this.pao,
+                    carne: this.carne,
+                    opcionais: Array.from(this.opcionais),
+                    status: 'Solicitado'
+                })
+            });
+            const data = await req.json();
+
+            //Colocar mensagem na tela
+            this.msg =`Pedido Nº ${data.id} criado com sucesso!`;
+
+            //limpar mensagem da tela
+            setTimeout(() => {
+                this.msg = null;
+            }, 3000);
+
+
+            //Limpar os campos do formulário
+            this.name = null;
+            this.pao = null;
+            this.carne = null;
+            this.opcionais = [];
+        },
+    },
+    mounted(){
+        this.getIngredientes();
+    },
+    components: {
+    Message,
+    Message
+},
+}
+</script>
+<style scoped>
+#burger-form{
+    max-width: 400px;
+    margin: 0 auto;
+}
+.input-container{
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
+label{
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: #222;
+    padding: 5px 10px;
+    border-left: 4px solid #FCBA03;
+}
+input, select{
+    padding: 5px 10px;
+    width: 300px;
+}
+
+#opcionais-container{
+   flex-direction: row;
+    flex-wrap: wrap;
+    
+}
+#opcionais-title{
+    width: 100%;
+}
+
+.checkbox-container{
+    display: flex;
+    align-items: flex-start;
+    width: 50%;
+    margin-bottom: 20px;
+}
+
+.checkbox-container span, .checkbox-container input{
+    width: auto;
+}
+
+.checkbox-container span{
+    margin-left: 6px;
+    font-weight: bold;
+}
+.submit-btn{
+    background-color: #222;
+    color: #FCBA03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+    border-radius: 5px;
+}
+.submit-btn:hover{
+    background-color: #FCBA03;
+    color: #222;
+}
+</style>
---
+++ battisti/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer id="footer">
+        <p> Make Your Burger &copy; 2022</p>
+    </footer>
+</template>
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+<style>
+#footer {
+    height: 250px;
+    background-color: #222;
+    color: #FCBA03;
+    border-top: 4px solid #111;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+
+}
+</style>
